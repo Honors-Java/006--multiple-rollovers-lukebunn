52cd1c9608a818994a953d92f95f25dd7180b2f6
lukebunn1 <superpantsftw1@gmail.com>
2020-11-06 15:44:57 +0000

    Added Green color. Inspired by one of the examples in class

--- index.java
+++ index.java
@@ -7,11 +7,30 @@ void draw() {
 	stroke(0);
 
   //start creating the lines
-
-
+  line(width/2,0,width/2,height);
+  line(0,height/2,500,height/2);
+  line(0,0,width,0);
+  line(0,0,0,height);
+  line(height - 1,0,height - 1,width - 1);
+  line(0, height - 1,height - 1,width - 1);
   //Fill a black color
+  if(mouseX < width/2 && mouseY < height/2){
+    fill(40,70,30);
+    rect(0,0,width/2,height/2);
+  }
+  if(mouseX > width/2 && mouseY < height/2){
+    fill(40,70,30);
+    rect(width/2,0,width,height/2);
+  }
+ if(mouseX < width/2 && mouseY > height/2){
+  fill(40,70,30);
+  rect(0,height/2,width/2,height);
+ }
+ if(mouseX > width/2 && mouseY > height/2){
+   fill(40,70,30);
+   rect(width/2,height/2,width,height);
+ }
   
-
   //Build your conditional statement 
 	
 
